02009112f57b6987a6e9b30cd260e370f31e6a4a
not-too-amused <kennaleeds@gmail.com>
2024-10-02 17:54:31 +0100

    adds sessionStorage util

---
+++ app/utils/sessionStorage.tsx
@@ -0,0 +1,14 @@
+import { createCookieSessionStorage } from "@remix-run/node";
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 7, // 1 week
+  },
+});
+
+export const { getSession, commitSession, destroySession } = sessionStorage;
