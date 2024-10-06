b98f325329bffc924a4d1a7ebb435995df9024ed
not-too-amused <kennaleeds@gmail.com>
2024-10-06 12:32:35 +0100

    adds QuantitySelector to ProductCard

--- app/components/productCard/ProductCard.tsx
+++ app/components/productCard/ProductCard.tsx
@@ -1,19 +1,11 @@
 import "./ProductCard.css";
 
-import { useEffect, useState } from "react";
-
 import { ProductFieldsFragment } from "types/storefront.generated";
-import { useShopifyClient } from "~/context/ShopifyClient";
+import QuantitySelector from "../quantitySelector/QuantitySelector";
 
 const ProductCard = ({ product }: { product: ProductFieldsFragment }) => {
   const initialVariant =
     product.variants.edges.length > 0 ? product.variants.edges[0].node.id : "";
-  const [selectedVariant, setSelectedVariant] = useState(initialVariant);
-  const [quantity, setQuantity] = useState<number>(0);
-  const [cartLineId, setCartLineId] = useState<string>("");
-  const client = useShopifyClient();
-
-  const variantQuantity = product.variants.edges[0].node.quantityAvailable ?? 0;
 
   return (
     <div className="product__card">
@@ -72,44 +64,7 @@ const ProductCard = ({ product }: { product: ProductFieldsFragment }) => {
       </div>
       <hr />
       <div className="product__card-si">
-        <div className="product__card-si-quantity">
-          <button type="button" onClick={() => {}}>
-            Minus
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="26"
-              viewBox="0 0 26 26"
-              fill="none"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8.25 14.5H18.75C19.164 14.5 19.5 14.164 19.5 13.75C19.5 13.336 19.164 13 18.75 13H8.25C7.836 13 7.5 13.336 7.5 13.75C7.5 14.164 7.836 14.5 8.25 14.5Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-          <span>{quantity}</span>
-          <button type="button" onClick={() => {}}>
-            Plus
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="41"
-              height="40"
-              viewBox="0 0 41 40"
-              fill="none"
-            >
-              <rect x="0.5" width="40" height="40" rx="20" fill="#E9ECF0" />
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M21.3333 10.8333C21.3333 10.3731 20.9602 10 20.5 10C20.0398 10 19.6667 10.3731 19.6667 10.8333V19.1667H11.3333C10.8731 19.1667 10.5 19.5398 10.5 20C10.5 20.4602 10.8731 20.8333 11.3333 20.8333H19.6667L19.6667 29.1667C19.6667 29.6269 20.0398 30 20.5 30C20.9602 30 21.3333 29.6269 21.3333 29.1667L21.3333 20.8333H29.6667C30.1269 20.8333 30.5 20.4602 30.5 20C30.5 19.5398 30.1269 19.1667 29.6667 19.1667H21.3333L21.3333 10.8333Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-        </div>
+        <QuantitySelector variantId={initialVariant} />
         <div className="product__card-si-price">
           {product.variants.edges[0]?.node.price.amount}
         </div>
